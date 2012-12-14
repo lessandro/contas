687d58af087c7de7915712d15bd8c805e04eca8d
Lessandro Mariano <lessandro@gmail.com>
2012-12-14 11:59:52 -0200

    couch.hs: Add comments to every function

--- src/couch.hs
+++ src/couch.hs
@@ -13,24 +13,29 @@ data Item = Item { blob, ref :: String }
 
 contas = db "contas"
 
+-- Transform a json string into an Ttem
 decodeItem :: String -> Item
 decodeItem = decodeJSON
 
+-- Get the whole document data
 getRaw :: String -> IO (Maybe String)
 getRaw ref = do
     raw <- runCouchDB' $ getDocRaw contas $ doc ref
     return raw
 
+-- Get the whole document data as an Item
 getItem ::  String -> IO (Maybe Item)
 getItem ref = do
     raw <- getRaw ref
     return $ liftM decodeItem $ raw
 
+-- Get the ref field of the current doc
 getCurrentRef :: IO (Maybe String)
 getCurrentRef = do
     item <- getItem "current"
     return $ liftM ref $ item
 
+-- Change the "current" doc ref to newRef
 updateCurrent :: String -> IO ()
 updateCurrent newRef = do
     theDoc <- runCouchDB' $ getDoc contas $ doc "current"
@@ -40,6 +45,9 @@ updateCurrent newRef = do
     Just (doc2, rev2) <- runCouchDB' $ updateDoc contas (doc1, rev1) json2
     return ()
 
+-- Add a new doc with given blob.
+-- The new doc points to the previous current doc and current now points
+-- to the new doc
 addNewBlob :: String -> IO ()
 addNewBlob newBlob = do
     Just current <- getCurrentRef
