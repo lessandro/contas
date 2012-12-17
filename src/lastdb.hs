fc93426139f4194689470d69769a9d8633e16019
Lessandro Mariano <lessandro@gmail.com>
2012-12-17 00:22:37 -0200

    lastdb.hs: A database that only cares about the last few entries

---
+++ src/lastdb.hs
@@ -0,0 +1,45 @@
+module LastDB where
+
+import System.IO
+
+db = withFile "db"
+
+formatThing :: (String, Integer) -> String
+formatThing (line, pos) = show pos ++ " " ++ line
+
+getCharAt :: Handle -> Integer -> IO Char
+getCharAt _ (-1) = return '\n'
+getCharAt h pos = do
+    hSeek h AbsoluteSeek pos
+    hLookAhead h
+
+-- Read a line backwards
+rGetLine :: Handle -> Integer -> IO (String, Integer)
+rGetLine h pos = do
+    c <- getCharAt h $ pos - 1
+    case c of
+        '\n' -> return ("", pos-1)
+        _ -> do
+            (ln, pos') <- rGetLine h $ pos - 1
+            return $ (ln ++ [c], pos')
+
+fileSize :: Handle -> IO Integer
+fileSize h = do
+    hSeek h SeekFromEnd 0
+    hTell h
+
+getThing :: Integer -> IO String
+getThing (-1) = return "-1"
+getThing pos = db ReadMode $ \h -> do
+    thing <- rGetLine h pos
+    return $ formatThing thing
+
+getLastThing :: IO String
+getLastThing = db ReadMode $ \h -> do
+    pos <- fileSize h
+    thing <- rGetLine h $ pos - 1
+    return $ formatThing thing
+
+appendThing :: String -> IO ()
+appendThing thing = db AppendMode $ \h -> do
+    hPutStrLn h thing
